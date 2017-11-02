de48a98bd798c53a11ea08254135353d53d7e984
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-11-01 22:32:31 -0400

    Create option to update name and email

--- script.py
+++ script.py
@@ -17,6 +17,14 @@ def insert_customer_details(value1, value2):
 	query = "INSERT INTO customer_info (customer_name, email) VALUES (?, ?)"
 	cursor.execute(query, (value1, value2))
 
+def update_customer_name(customer_name, customer_id):
+	query = "UPDATE customer_info SET customer_name = ? WHERE shopper_id = ?"
+	cursor.execute(query, (customer_name, customer_id))
+
+def update_customer_email(customer_email, customer_id):
+	query = "UPDATE customer_info SET email = ? WHERE shopper_id = ?"
+	cursor.execute(query, (customer_email, customer_id))
+
 def create_table_catalogue():
 	query = "CREATE TABLE IF NOT EXISTS catalogue_info (item_id INTEGER PRIMARY KEY, item_name TEXT, brand TEXT)"
 	cursor.execute(query)
@@ -31,10 +39,17 @@ def save_and_close_db():
 
 def main():
 	create_table_customers()
-	# display_customer_table()
-	name = input("What is your name?\n")
-	email = input("What is your email?\n")
-	insert_customer_details(name, email)
+	display_customer_table()
+	# name = input("What is your name?\n")
+	# email = input("What is your email?\n")
+	# insert_customer_details(name, email)
+
+	shopper_id = input("Which id do you want to change?\n")
+	# new_name = input("What is the new name you want to enter?\n")
+	# update_customer_name(new_name, int(shoppers_id))
+	new_email = input("What is the new email of this person?\n")
+	update_customer_email(new_email, int(shopper_id))
+
 	save_and_close_db()
 
 # create_table_catalogue()
