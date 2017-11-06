a9bda051bc567baf140db39a3ac21db7d38f91fd
Sreedevi Krishnan <sreedevik29@gmail.com>
2017-11-05 22:06:32 -0500

    Add delete optopn for customer details and CRUD for catalogue_details

--- script.py
+++ script.py
@@ -11,7 +11,7 @@ def display_customer_table():
 	query = "SELECT * FROM customer_info"
 	table = cursor.execute(query)
 	for row in table:
-		print("[" + str(row[0]) + "]" + "[" + " -- [" + row[1] + "]" + " -- [" + str(row[2]) + "]")
+		print("[" + str(row[0]) + "]" + "[" + " -- [" + row[1] + "]" + " -- [" + row[2] + "]")
 
 def insert_customer_details(value1, value2):
 	query = "INSERT INTO customer_info (customer_name, email) VALUES (?, ?)"
@@ -25,9 +25,27 @@ def update_customer_email(customer_email, customer_id):
 	query = "UPDATE customer_info SET email = ? WHERE shopper_id = ?"
 	cursor.execute(query, (customer_email, customer_id))
 
+def delete_customer(shopper_id):
+	query = "DELETE FROM customer_info WHERE id = ?"
+	cursor.execute(query, (shopper_id,))
+
 def create_table_catalogue():
-	query = "CREATE TABLE IF NOT EXISTS catalogue_info (item_id INTEGER PRIMARY KEY, item_name TEXT, brand TEXT)"
+	query = "CREATE TABLE IF NOT EXISTS catalogue_info (item_id INTEGER PRIMARY KEY, item_name TEXT, type_of_clothing TEXT, sold_status BOOLEAN DEFAULT 1)"
 	cursor.execute(query)
+
+def display_table_catalogue():
+	query = "SELECT * FROM catalogue_info"
+	table = cursor.execute(query)
+	for row in table:
+		print("[" + str(row[0]) + "]" + "[" + " -- [" + row[1] + "]" + " -- [" + row[2] + "]" + " -- [" + str(row[3]) + "]")
+
+def insert_catalogue_details(value1, value2):
+	query = "INSERT INTO catalogue_info (item_name, type_of_clothing) VALUES (?, ?)"
+	cursor.execute(query, (value1, value2))
+
+def update_catalogue_details(item_id)
+	query = "UPDATE catalogue_info SET sold_status = 0 WHERE id = ?"
+	cursor.execute(query, (item_id,))
 
 def create_table_purchases():
 	query = "CREATE TABLE IF NOT EXISTS purchases_info (purchases_id INTEGER PRIMARY KEY, date_time DATETIME, FOREIGN KEY (item_id) REFERENCES create_table_catalogue(item_id)"
@@ -43,12 +61,15 @@ def main():
 	# name = input("What is your name?\n")
 	# email = input("What is your email?\n")
 	# insert_customer_details(name, email)
+	# shopper_id = input("Which id do you want to change?\n")
+	# # new_name = input("What is the new name you want to enter?\n")
+	# # update_customer_name(new_name, int(shoppers_id))
+	# new_email = input("What is the new email of this person?\n")
+	# update_customer_email(new_email, int(shopper_id))
 
-	shopper_id = input("Which id do you want to change?\n")
-	# new_name = input("What is the new name you want to enter?\n")
-	# update_customer_name(new_name, int(shoppers_id))
-	new_email = input("What is the new email of this person?\n")
-	update_customer_email(new_email, int(shopper_id))
+	create_table_catalogue()
+	display_table_catalogue()
+	insert_catalogue_details()
 
 	save_and_close_db()
 
